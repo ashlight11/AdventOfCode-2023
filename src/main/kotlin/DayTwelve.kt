ea28b1d004f344a217317f87658830f2c564e701
Marianne De Poorter <mariannedepoorter@hotmail.fr>
2024-04-16 16:50:14 +0200

    Messy commit with my own "math" research, plus a Dynamic programming bottom-up approach found here https://www.reddit.com/r/adventofcode/comments/18ge41g/2023_day_12_solutions/ as proposed by Nithramir

--- src/main/kotlin/DayTwelve.kt
+++ src/main/kotlin/DayTwelve.kt
@@ -1,5 +1,68 @@
-fun main(args: Array<String>) {
+import java.io.File
+import kotlin.math.ceil
 
+fun main(args: Array<String>) {
+    val url = object {}.javaClass.getResource("day12.txt")
+    val file = File(url!!.toURI())
+    val sumOfArrangements = file.readLines()
+        .parseToCharArrayAndBooleanArray(repeatingFactor = 5, addTextForString = "?", ",")
+        .map { countArrangements(it.key, it.value) }
+        .sum()
+    println(sumOfArrangements)
+
+}
+
+fun List<String>.parseToMap(): Map<String, List<Int>> {
+    return this.map {
+        val split = it.split(" ");
+        split[0] to split[1].toListOfInts()
+    }.toMap()
+}
+
+fun List<String>.parseToCharArrayAndBooleanArray(repeatingFactor: Int, addTextForString: String?, addTextForNums: String?): Map<CharArray, BooleanArray> {
+    return this.map {
+        val split = it.split(" ");
+        val text = split[0].repeatNTimesAndAddInBetween(repeatingFactor, addTextForString)
+            .toCharArray()
+            .appendAtFirstAndAtLast(charArrayOf('.'))
+        val nums = split[1].repeatNTimesAndAddInBetween(repeatingFactor, addTextForNums)
+            .toListOfInts()
+            .toBooleanArray()
+        text to nums
+    }.toMap()
+}
+
+fun String.repeatNTimesAndAddInBetween(repeatingFactor: Int, addText: String?): String {
+    return if(addText.isNullOrBlank()){
+        this.repeat(repeatingFactor)
+    } else {
+        (this + addText).repeat(repeatingFactor).removeSuffix(addText)
+    }
+
+}
+
+fun CharArray.appendAtFirstAndAtLast(characterArray: CharArray): CharArray {
+    return characterArray + this + characterArray
+}
+
+fun String.toListOfInts(): List<Int> {
+    return this.split(",").map { it.toInt() }
+}
+
+fun List<Int>.toBooleanArray(): BooleanArray {
+    val result = BooleanArray(this.sum() + 2 + this.size - 1)
+    var index = 1
+    this.forEach {
+        (index..<index + it).forEach { idx ->
+            result[idx] = true
+        }
+        index = index + it
+        result[index] = false
+        index++
+    }
+    result[0] = false
+    result[result.size - 1] = false
+    return result
 }
 
 data class Spring(val value: Char) {
@@ -22,17 +85,17 @@ class SpringGroup(val springs: List<Spring>) {
 
 // not restricted = we don't care separating # with "."
 // if k elements are already placed : elementsToPlace - k among n-k
-fun SpringGroup.permutationsNonRestricted(elementsToPlace: Int) : Int {
+fun SpringGroup.permutationsNonRestricted(elementsToPlace: Int): Int {
     val n = springs.size
     val num = factorialTailRecursive(n - numberOfDamaged)
-    val dem = factorialTailRecursive(n- elementsToPlace - numberOfDamaged)
+    val dem = factorialTailRecursive(n - elementsToPlace - numberOfDamaged)
     return num.div(dem)
 }
 
-fun SpringGroup.permutationsWithRestriction(elementsToPlace: Int, restrictionsToPlace: Int) : Int {
+fun SpringGroup.permutationsWithRestriction(elementsToPlace: Int, restrictionsToPlace: Int): Int {
     val n = springs.size - restrictionsToPlace
     val num = factorialTailRecursive(n - numberOfDamaged)
-    val dem = factorialTailRecursive(n- elementsToPlace - numberOfDamaged)
+    val dem = factorialTailRecursive(n - elementsToPlace - numberOfDamaged)
     return num.div(dem)
 }
 
@@ -44,7 +107,23 @@ fun String.toSprings(): List<Spring> {
  * The number of total permutations possible is :
  * nbOfGroupsOfGivenSizePossibleInWholeLine - nbOfGroupsOfSaidSizeAlreadyPresentInLine
  * - (nbOfElementsLeftToPlace / groupSize) - nbOfWorkingSpringsLeftToPlace
+ *
+ * groupsLeftToPlace  = (nbOfElementsLeftToPlace / groupSize)
  */
+fun String.getTotalPermutationsForGroup(groupSize: Int, remainingGroupsToPlace: List<Int>): Int {
+    val toSubstract = ceil((remainingGroupsToPlace.size - 1 + remainingGroupsToPlace.sum()).toFloat() / groupSize)
+    val nbOfGroupsOfGivenSizePossibleInWholeLine = this.length / groupSize
+    val nbOfGroupsOfSaidSizeAlreadyPresentInLine = this.findGroupsOfSize(groupSize)
+    return nbOfGroupsOfGivenSizePossibleInWholeLine - nbOfGroupsOfSaidSizeAlreadyPresentInLine - toSubstract.toInt()
+}
+
+// [.?]#{$groupSize}[.?]
+fun String.findGroupsOfSize(groupSize: Int): Int {
+    val regex = Regex("[.?]#{$groupSize}[.?]")
+    return regex.findAll(this).count()
+
+}
+
 
 enum class SpringState {
     DAMAGED,
@@ -53,3 +132,38 @@ enum class SpringState {
     ERROR
 }
 
+fun countArrangements(chars: CharArray, springs: BooleanArray): Long {
+    val n = chars.size
+    val m = springs.size
+    val dp = Array(n + 1) { LongArray(m + 1) }
+    dp[n][m] = 1
+
+    for (i in n - 1 downTo 0) {
+        for (j in m - 1 downTo 0) {
+            var damaged = false
+            var operational = false
+            when (chars[i]) {
+                '#' -> {
+                    damaged = true
+                }
+
+                '.' -> {
+                    operational = true
+                }
+
+                else -> {
+                    operational = true
+                    damaged = true
+                }
+            }
+            var sum: Long = 0
+            if (damaged && springs[j]) {
+                sum += dp[i + 1][j + 1]
+            } else if (operational && !springs[j]) {
+                sum += dp[i + 1][j + 1] + dp[i + 1][j]
+            }
+            dp[i][j] = sum
+        }
+    }
+    return dp[0][0]
+}
--- src/test/kotlin/DayTwelveTest.kt
+++ src/test/kotlin/DayTwelveTest.kt
@@ -3,20 +3,28 @@ import org.junit.jupiter.api.Test
 
 class DayTwelveTest {
     val corruptedInput =
-                "???.### 1,1,3\n" +
-                ".??..??...?##. 1,1,3\n" +
-                "?#?#?#?#?#?#?#? 1,3,1,6\n" +
-                "????.#...#... 4,1,1\n" +
-                "????.######..#####. 1,6,5\n" +
-                "?###???????? 3,2,1"
+            "???.### 1,1,3\n" +
+            ".??..??...?##. 1,1,3\n" +
+            "?#?#?#?#?#?#?#? 1,3,1,6\n" +
+            "????.#...#... 4,1,1\n" +
+            "????.######..#####. 1,6,5\n" +
+            "?###???????? 3,2,1"
 
     @Test
-    fun shouldMapRows(){
-
+    fun shouldMapRows() {
+        val expectedMap = mapOf(
+            "???.###" to listOf(1, 1, 3),
+            ".??..??...?##." to listOf(1, 1, 3),
+            "?#?#?#?#?#?#?#?" to listOf(1, 3, 1, 6),
+            "????.#...#..." to listOf(4, 1, 1),
+            "????.######..#####." to listOf(1, 6, 5),
+            "?###????????" to listOf(3, 2, 1)
+        )
+        corruptedInput.lines().parseToMap().shouldBe(expectedMap)
     }
 
     @Test
-    fun shouldFindNumberOfPermutationsInNonRestrictedGroupWithFixedElements(){
+    fun shouldFindNumberOfPermutationsInNonRestrictedGroupWithFixedElements() {
         val testGroup1 = SpringGroup("?##".toSprings())
         val testGroup2 = SpringGroup("?###".toSprings())
         testGroup1.permutationsNonRestricted(1).shouldBe(1)
@@ -24,7 +32,7 @@ class DayTwelveTest {
     }
 
     @Test
-    fun shouldFindNumberOfPermutationsInNonRestrictedGroup(){
+    fun shouldFindNumberOfPermutationsInNonRestrictedGroup() {
         val testGroup1 = SpringGroup("????".toSprings())
         val testGroup2 = SpringGroup("??".toSprings())
         testGroup1.permutationsNonRestricted(1).shouldBe(4)
@@ -32,7 +40,7 @@ class DayTwelveTest {
     }
 
     @Test
-    fun shouldFindNumberOfPermutationsInGroupToRestrict(){
+    fun shouldFindNumberOfPermutationsInGroupToRestrict() {
         val testGroup1 = SpringGroup("???????".toSprings())
         // have to place 2 THEN 1
         // first is 2 among n (=7) - [1 (number left to place) + 1 (reserved for '.', because not last to place)]
@@ -44,9 +52,47 @@ class DayTwelveTest {
     }
 
     @Test
-    fun shouldFindNbOfArrangementsForSingleLine(){
-        val line = "????.######..#####." // 1,6,5
+    fun shouldCountGroupsOfN() {
+        "????.######..#####.".findGroupsOfSize(5).shouldBe(1)
+    }
 
+    @Test
+    fun shouldGetNumberOfPermutationsPossible() {
+        val line = "?###????????" // 3, 2 , 1
+        line.getTotalPermutationsForGroup(2, listOf(1)).shouldBe(4)
+    }
+
+    @Test
+    fun shouldCountArrangements() {
+        countArrangements(
+            ".?###????????.".toCharArray(),
+            booleanArrayOf(false, true, true, true, false, true, true, false, true, false)
+        ).shouldBe(10)
+    }
+
+    @Test
+    fun intListToBooleanArray() {
+        listOf(3, 2, 1).toBooleanArray()
+            .shouldBe(booleanArrayOf(false, true, true, true, false, true, true, false, true, false))
+    }
+
+    @Test
+    fun shouldFindTotal() {
+        corruptedInput.lines().parseToCharArrayAndBooleanArray(1, null, null)
+            .map { countArrangements(it.key, it.value) }
+            .sum().shouldBe(21)
+    }
+
+    @Test
+    fun shouldReplicateStringNTimes(){
+        "test".repeatNTimesAndAddInBetween(3, "?.").shouldBe("test?.test?.test")
+    }
+
+    @Test
+    fun shouldFindTotalFor5TimesLonger(){
+        corruptedInput.lines().parseToCharArrayAndBooleanArray(5, "?", ",")
+            .map { countArrangements(it.key, it.value) }
+            .sum().shouldBe(525152)
     }
 
 }
